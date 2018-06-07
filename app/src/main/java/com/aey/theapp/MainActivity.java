b330b7a4fe5cb7fae3bc824e2f364fc20ac0c2da
Omar <oa.azab@gmail.com>
2018-06-07 09:33:19 +0200

    Add Map with location layer

--- app/src/main/java/com/aey/theapp/MainActivity.java
+++ app/src/main/java/com/aey/theapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.aey.theapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startTripBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startTripBtn = findViewById(R.id.btn_start_trip);
+        startTripBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start Maps Activity
+                Intent mapsInent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(mapsInent);
+            }
+        });
     }
 }
